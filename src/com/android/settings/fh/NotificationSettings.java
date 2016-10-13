16829dc299073a5e053771b594d7a36baace4d15
xyyx <xyyx@mail.ru>
2016-10-13 09:36:35 +0200

    Custom small QS tiles [2/2]
    Change-Id: I42d7df7a3f8a3ddc562fdf32caf27402c1b5aba5
    Signed-off-by: Scott <scott@ubuntu.com>
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/settings/fh/NotificationSettings.java
+++ src/com/android/settings/fh/NotificationSettings.java
@@ -39,10 +39,12 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
 private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
 private static final String PREF_COLUMNS = "qs_columns";
+private static final String KEY_SYSUI_QQS_COUNT = "sysui_qqs_count_key";
 
 private ListPreference mRowsPortrait;
 private ListPreference mRowsLandscape;
 private ListPreference mQsColumns;
+private ListPreference mSysuiQqsCount;
 
 public class NotificationSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -83,6 +85,13 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
         mQsColumns.setValue(String.valueOf(columnsQs));
         mQsColumns.setSummary(mQsColumns.getEntry());
         mQsColumns.setOnPreferenceChangeListener(this);
+
+        mSysuiQqsCount = (ListPreference) findPreference(KEY_SYSUI_QQS_COUNT);
+        int SysuiQqsCount = Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.QQS_COUNT, 5);
+        mSysuiQqsCount.setValue(Integer.toString(SysuiQqsCount));
+        mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
+        mSysuiQqsCount.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -114,6 +123,13 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.QS_COLUMNS, intValue);
             preference.setSummary(mQsColumns.getEntries()[index]);
+            return true;
+        } else if (preference == mSysuiQqsCount) {
+            String SysuiQqsCount = (String) objValue;
+            int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+            int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
+            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
             return true;
         }
         return false;
