e2173543e9725a8074c33242a5afd6a2bad94f49
bigrushdog <randall.rushing@gmail.com>
2016-10-15 19:11:42 +0200

    DUI: Initial N checkin
    - Put all DUI settings under one commit
    
    Change-Id: I98e639868eeb4c5d5496f73cd6c67345c7c2e57b
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -62,6 +62,10 @@ import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.accounts.ChooseAccountActivity;
 import com.android.settings.accounts.ManagedProfileSettings;
+import com.android.settings.fh.FlingSettings;
+import com.android.settings.fh.NavbarSettings;
+import com.android.settings.fh.SmartbarSettings;
+import com.android.settings.fh.PulseSettings;
 import com.android.settings.applications.AdvancedAppSettings;
 import com.android.settings.applications.DrawOverlayDetails;
 import com.android.settings.applications.InstalledAppDetails;
@@ -370,11 +374,15 @@ public class SettingsActivity extends SettingsDrawerActivity
             TestingSettings.class.getName(),
             WifiAPITest.class.getName(),
             WifiInfo.class.getName(),
+            FhSettings.class.getName(),
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName(),
+            PulseSettings.class.getName(),
             MasterClear.class.getName(),
             NightDisplaySettings.class.getName(),
             ManageDomainUrls.class.getName(),
-            AutomaticStorageManagerSettings.class.getName(),
-            FhSettings.class.getName()
+            AutomaticStorageManagerSettings.class.getName()
     };
 
 
