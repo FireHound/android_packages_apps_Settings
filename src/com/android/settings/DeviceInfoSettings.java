a79004f366628f8a24adf9a2ea16c22f06df04a0
PMS22 <prathams99@rediff.com>
2017-03-09 16:02:24 +0000

    About Phone: Expose Android SDK
    Change-Id: Idde7b0a857e879a194109bea3bc29489ae2aa232
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -83,6 +83,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_MOD_VERSION = "mod_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
     private static final String KEY_MOD_API_LEVEL = "mod_api_level";
+    private static final String BUILD_SDK = "build_sdk";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -149,6 +150,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_MOD_VERSION, "ro.fh.display.version");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        setValueSummary(BUILD_SDK, "ro.build.version.sdk");
         setExplicitValueSummary(KEY_MOD_API_LEVEL, constructApiLevelString());
         findPreference(KEY_MOD_API_LEVEL).setEnabled(true);
 
