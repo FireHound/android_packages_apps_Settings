5fc6450f05f77e2248151585c65c255b22e14aa6
PMS22 <prathams99@rediff.com>
2017-03-11 12:56:49 +0000

    SwitchPreference: Fix path
    Change-Id: I55dc88c9c992323e7a84083d375ee32e89b6c604
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/settings/fh/SecureSettingSwitchPreference.java
+++ src/com/android/settings/fh/SecureSettingSwitchPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.rr.Preferences;
+package com.android.settings.fh;
 
 import android.content.Context;
 import android.support.v14.preference.SwitchPreference;
