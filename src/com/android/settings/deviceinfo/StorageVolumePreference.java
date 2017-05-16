87f630b27d2a30b4c3dca616200749ed6dfac7d4
PMS22 <prathams99@rediff.com>
2017-05-16 02:55:55 +0000

    Overall fixes

--- src/com/android/settings/deviceinfo/StorageVolumePreference.java
+++ src/com/android/settings/deviceinfo/StorageVolumePreference.java
@@ -20,7 +20,6 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
-import android.graphics.PorterDuff;
 import android.os.storage.StorageManager;
 import android.os.storage.VolumeInfo;
 import android.support.v7.preference.Preference;
@@ -28,7 +27,6 @@ import android.support.v7.preference.PreferenceViewHolder;
 import android.text.format.Formatter;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.util.TypedValue;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
@@ -46,7 +44,6 @@ public class StorageVolumePreference extends Preference {
     private final VolumeInfo mVolume;
 
     private int mColor;
-    private int mSecondaryColor;
     private int mUsedPercent = -1;
 
     // TODO: ideally, VolumeInfo should have a total physical size.
@@ -56,10 +53,6 @@ public class StorageVolumePreference extends Preference {
         mStorageManager = context.getSystemService(StorageManager.class);
         mVolume = volume;
         mColor = color;
-
-        TypedValue typedValue = new TypedValue();
-        context.getTheme().resolveAttribute(android.R.attr.textColorSecondary, typedValue, true);
-        mSecondaryColor = context.getResources().getColor(typedValue.resourceId);
 
         setLayoutResource(R.layout.storage_volume);
 
@@ -90,7 +83,7 @@ public class StorageVolumePreference extends Preference {
             }
 
             if (freeBytes < mStorageManager.getStorageLowBytes(path)) {
-                mColor = StorageSettings.getColorWarning(context.getResources());
+                mColor = StorageSettings.COLOR_WARNING;
                 icon = context.getDrawable(R.drawable.ic_warning_24dp);
             }
 
@@ -101,7 +94,6 @@ public class StorageVolumePreference extends Preference {
 
         icon.mutate();
         icon.setTint(mColor);
-        icon.setTintMode(PorterDuff.Mode.SRC_ATOP);
         setIcon(icon);
 
         if (volume.getType() == VolumeInfo.TYPE_PUBLIC
@@ -115,7 +107,7 @@ public class StorageVolumePreference extends Preference {
     public void onBindViewHolder(PreferenceViewHolder view) {
         final ImageView unmount = (ImageView) view.findViewById(R.id.unmount);
         if (unmount != null) {
-            unmount.setImageTintList(ColorStateList.valueOf(mSecondaryColor));
+            unmount.setImageTintList(ColorStateList.valueOf(Color.parseColor("#8a000000")));
             unmount.setOnClickListener(mUnmountListener);
         }
 
