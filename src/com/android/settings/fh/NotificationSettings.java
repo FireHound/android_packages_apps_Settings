571c3f83b09f640a7d2727e6fcffae210087be88
PMS22 <prathams99@rediff.com>
2017-06-08 05:08:22 +0000

    Settings: Revamp UI
    Change-Id: Ibf83cce6c3cc46b89ba23eeb851347428a85b86e
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- src/com/android/settings/fh/NotificationSettings.java
+++ src/com/android/settings/fh/NotificationSettings.java
@@ -67,10 +67,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
         private static final String CUSTOM_HEADER_PROVIDER = "custom_header_provider";
         private static final String CUSTOM_HEADER_BROWSE = "custom_header_browse";
 
-        private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
-        private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
-        private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
-
         private CustomSeekBarPreference mSysuiQqsCount;
         private CustomSeekBarPreference mRowsPortrait;
         private CustomSeekBarPreference mRowsLandscape;
@@ -81,10 +77,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
         private ListPreference mHeaderProvider;
         private String mDaylightHeaderProvider;
         private PreferenceScreen mHeaderBrowse;
-
-        private ListPreference mTileAnimationStyle;
-        private ListPreference mTileAnimationDuration;
-        private ListPreference mTileAnimationInterpolator;
 
  	private static final String DISABLE_IMMERSIVE_MESSAGE = "disable_immersive_message";
 
@@ -105,31 +97,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
                 DISABLE_IMMERSIVE_MESSAGE, 0);
 	mDisableIM.setChecked(DisableIM != 0);
         int defaultValue;
-
-        mTileAnimationStyle = (ListPreference) findPreference(PREF_TILE_ANIM_STYLE);
-        int tileAnimationStyle = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.ANIM_TILE_STYLE, 0,
-                UserHandle.USER_CURRENT);
-        mTileAnimationStyle.setValue(String.valueOf(tileAnimationStyle));
-        updateTileAnimationStyleSummary(tileAnimationStyle);
-        updateAnimTileStyle(tileAnimationStyle);
-        mTileAnimationStyle.setOnPreferenceChangeListener(this);
-
-        mTileAnimationDuration = (ListPreference) findPreference(PREF_TILE_ANIM_DURATION);
-        int tileAnimationDuration = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.ANIM_TILE_DURATION, 2000,
-                UserHandle.USER_CURRENT);
-        mTileAnimationDuration.setValue(String.valueOf(tileAnimationDuration));
-        updateTileAnimationDurationSummary(tileAnimationDuration);
-        mTileAnimationDuration.setOnPreferenceChangeListener(this);
-
-        mTileAnimationInterpolator = (ListPreference) findPreference(PREF_TILE_ANIM_INTERPOLATOR);
-        int tileAnimationInterpolator = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.ANIM_TILE_INTERPOLATOR, 0,
-                UserHandle.USER_CURRENT);
-        mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
-        updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
-        mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
 
         mRowsPortrait = (CustomSeekBarPreference) findPreference(PREF_ROWS_PORTRAIT);
         int rowsPortrait = Settings.Secure.getInt(resolver,
@@ -213,25 +180,6 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(), DISABLE_IMMERSIVE_MESSAGE,
                     value ? 1 : 0);
             return true;
-        } else if (preference == mTileAnimationStyle) {
-            int tileAnimationStyle = Integer.valueOf((String) objValue);
-            Settings.System.putIntForUser(getContentResolver(), Settings.System.ANIM_TILE_STYLE,
-                    tileAnimationStyle, UserHandle.USER_CURRENT);
-            updateTileAnimationStyleSummary(tileAnimationStyle);
-            updateAnimTileStyle(tileAnimationStyle);
-            return true;
-        } else if (preference == mTileAnimationDuration) {
-            int tileAnimationDuration = Integer.valueOf((String) objValue);
-            Settings.System.putIntForUser(getContentResolver(), Settings.System.ANIM_TILE_DURATION,
-                    tileAnimationDuration, UserHandle.USER_CURRENT);
-            updateTileAnimationDurationSummary(tileAnimationDuration);
-            return true;
-        } else if (preference == mTileAnimationInterpolator) {
-            int tileAnimationInterpolator = Integer.valueOf((String) objValue);
-            Settings.System.putIntForUser(getContentResolver(), Settings.System.ANIM_TILE_INTERPOLATOR,
-                    tileAnimationInterpolator, UserHandle.USER_CURRENT);
-            updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
-            return true;
         } else if (preference == mRowsPortrait) {
             int rowsPortrait = (Integer) objValue;
             Settings.Secure.putInt(getActivity().getContentResolver(),
@@ -281,37 +229,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements
         return MetricsEvent.APPLICATION;
     }
 
-    private void updateTileAnimationStyleSummary(int tileAnimationStyle) {
-        String prefix = (String) mTileAnimationStyle.getEntries()[mTileAnimationStyle.findIndexOfValue(String
-                .valueOf(tileAnimationStyle))];
-        mTileAnimationStyle.setSummary(getResources().getString(R.string.qs_set_animation_style, prefix));
-    }
-
-    private void updateTileAnimationDurationSummary(int tileAnimationDuration) {
-        String prefix = (String) mTileAnimationDuration.getEntries()[mTileAnimationDuration.findIndexOfValue(String
-                .valueOf(tileAnimationDuration))];
-        mTileAnimationDuration.setSummary(getResources().getString(R.string.qs_set_animation_duration, prefix));
-    }
-
-    private void updateTileAnimationInterpolatorSummary(int tileAnimationInterpolator) {
-        String prefix = (String) mTileAnimationInterpolator.getEntries()[mTileAnimationInterpolator.findIndexOfValue(String
-                .valueOf(tileAnimationInterpolator))];
-        mTileAnimationInterpolator.setSummary(getResources().getString(R.string.qs_set_animation_interpolator, prefix));
-    }
-
-    private void updateAnimTileStyle(int tileAnimationStyle) {
-        if (mTileAnimationDuration != null) {
-            if (tileAnimationStyle == 0) {
-                mTileAnimationDuration.setSelectable(false);
-                mTileAnimationInterpolator.setSelectable(false);
-            } else {
-                mTileAnimationDuration.setSelectable(true);
-                mTileAnimationInterpolator.setSelectable(true);
-            }
-        }
-    }
-
-      private void getAvailableHeaderPacks(List<String> entries, List<String> values) {
+    private void getAvailableHeaderPacks(List<String> entries, List<String> values) {
               Intent i = new Intent();
               PackageManager packageManager = getActivity().getPackageManager();
               i.setAction("org.omnirom.DaylightHeaderPack");
