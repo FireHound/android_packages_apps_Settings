a514931e9ec85f6052cf91b80f8db543360f6014
Harsh Shandilya <harsh@prjkt.io>
2018-01-08 18:27:49 +0530

    DeviceInfoSettings: Add FireHound version
    Change-Id: Ie875701d39e88d27ddc3565e9fd0cf7f475d5639
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -35,6 +35,7 @@ import com.android.settings.deviceinfo.FirmwareVersionPreferenceController;
 import com.android.settings.deviceinfo.KernelVersionPreferenceController;
 import com.android.settings.deviceinfo.ManualPreferenceController;
 //import com.android.settings.deviceinfo.RegulatoryInfoPreferenceController;
+import com.android.settings.deviceinfo.ROMVersionPreferenceController;
 import com.android.settings.deviceinfo.SELinuxStatusPreferenceController;
 import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
 import com.android.settings.deviceinfo.SecurityPatchPreferenceController;
@@ -133,6 +134,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new SELinuxStatusPreferenceController(context));
         controllers.add(new SafetyInfoPreferenceController(context));
         controllers.add(new VendorVersionPreferenceController(context));
+        controllers.add(new ROMVersionPreferenceController(context));
         return controllers;
     }
 
