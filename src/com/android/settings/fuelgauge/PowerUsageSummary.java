ca1f94fd7f52004031be3be8b03d984394733cdb
Alex Cruz <du.alexcruz@gmail.com>
2018-04-17 21:28:11 -0500

    Add a preference controller for Smart Pixels
    This keeps things uniformed with other features that we have in settings.
    Plus this avoids an NPE that's caused by checking for a preference in DT
    while the actual preference is in Settings.
    
    Change-Id: I171c0f0b962c2219d7ae0bd5c8a1d7d58d8cbb9c

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -325,6 +325,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
         //        context,
         //        new AmbientDisplayConfiguration(context),
         //        KEY_AMBIENT_DISPLAY));
+        controllers.add(new SmartPixelsPreferenceController(context));
         return controllers;
     }
 
